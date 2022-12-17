993f90f1c108c81c096991aa1ff306270e95fba9
Katya Panko <katena-1990@mail.ru>
2022-12-17 16:24:53 +0300

    JSON

---
+++ data.js
@@ -0,0 +1,10 @@
+const dataJson = `[{
+    "title": "JSON:API paints my bikeshed!",
+    "body": "The shortest article. Ever.",
+    "created": "2015-05-22T14:56:29.000Z",
+    "updated": "2015-05-22T14:56:28.000Z",
+    "img_src":"http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG",
+    "earth_date":"2015-05-30"
+    
+    }
+]`
